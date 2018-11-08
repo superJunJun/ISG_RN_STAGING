bd36913bba39e5ff5ef78020a81f26777f92c8d5
lijun <lijun@ishanggang.com>
2018-11-08 18:16:02 +0800

    first commit

---
+++ android/app/src/main/java/com/isg_rn_staging/MainActivity.java
@@ -0,0 +1,15 @@
+package com.isg_rn_staging;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "iSG_RN_staging";
+    }
+}
---
+++ common_dependence.js
@@ -0,0 +1,38 @@
+// import'react';
+// import'react-native';
+
+// /* third  libraries*/
+// import '@babel/core';
+// import 'eslint';
+// import 'js-base64';//base64编码
+// import 'native-echarts';//图表
+// import 'prop-types';
+// import 'react-native-calendar';//日历
+// import 'react-native-camera';//选择照片
+// import 'react-native-device-info/core';//获取设备信息
+// import 'react-native-elements';//UI框架
+// import 'react-native-image-crop-picker';//获取照片、裁剪
+// import 'react-native-image-zoom-viewer';//放大缩小image
+// import 'react-native-picker';//数据（地址、时间）选择器
+// import 'react-native-refreshable-listview';//下拉刷新listview
+// import 'react-native-render-html';//加载HTML文本信息
+// import 'react-native-root-toast'; //Toast
+// import 'react-native-scrollable-tab-view';//滑动切换
+// import 'react-native-snap-carousel';//轮播图
+// import 'react-native-storage';//数据存储
+// import 'react-native-swiper';//轮播组件
+// import 'react-native-vector-icons';//图标的使用
+// import 'react-native-view-shot';//截图
+// import 'react-navigation';//导航
+// import 'react-redux';//redux
+// import 'react-native-keyboard-aware-scroll-view';//TextInput输入键盘适应
+// import 'redux';
+// import 'redux-logger';
+// import 'react-thunk';//redux中间件
+// import 'uglify-es';
+
+import'react';
+import'react-native';
+
+/* third  libraries*/
+import 'react-redux';
